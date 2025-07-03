50267b624b187a9c702bf2048dcd367906c1dd09
unknown <skuratowaanna@yandex.ru>
2025-07-03 13:08:44 +0300

    blink_PB2

--- 01_STM32F103C8/000_GPIO/00_Blink_PB2.c
+++ 01_STM32F103C8/000_GPIO/00_Blink_PB2.c
@@ -1,3 +1,22 @@
+/**-------------------------------------------------------------------
+ \date  02.07.2025
+ *
+ *   STM32F103C8T6    
+ *   ------------ 
+ *  |            |
+ *  |            |
+ *  |            |
+ *  |        PB.2| ---->  LED
+ *  |            | 
+ *  |            |
+ *  |            |
+ *  |      +3.3V |
+ *  |        GND |
+ *
+ *\ authors        ScuratovaAnna
+ *\ сode debugging ScuratovaAnna 
+ */
+//**************************  Пример первый **************************
 #include "SEGGER_RTT.h"
 #include "stm32f10x.h"
 
@@ -6,16 +25,18 @@
 #define Output_mode_max_speed_2_MHz  (0x2UL)
 #define Output_mode_max_speed_50_MHz (0x3UL)
 
-#define output_push_pull            (0x0UL)
-#define output_Open_drain           (0x1UL)
-#define Alternate_output_Push_pull  (0x2UL)
-#define Alternate_output_Open_drain (0x3UL)
+#define output_push_pull             (0x0UL)
+#define output_Open_drain            (0x1UL)
+#define Alternate_output_Push_pull   (0x2UL)
+#define Alternate_output_Open_drain  (0x3UL)
 
 #define GPIO_CRL_MODE2_Pos (8U)
 #define GPIO_CRL_MODE2_Msk (0x3U << GPIO_CRL_MODE2_Pos)
 
-#define GPIO_CRL_CNF2_Pos (10U)
-#define GPIO_CRL_CNF2_Msk (0x3U << GPIO_CRL_CNF2_Pos)
+#define GPIO_CRL_CNF2_Pos  (10U)
+#define GPIO_CRL_CNF2_Msk  (0x3U << GPIO_CRL_CNF2_Pos)
+
+volatile u32 i;
 
 void PORTB_2_INIT(void) {
   // RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
@@ -36,14 +57,18 @@ int main(void) {
   while (1) {
     // GPIOB->BSRR |= GPIO_BSRR_BS2;
     SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS2);
-    for (volatile uint32_t i = 0; i < 1000000; i++)
+    for ( i = 0; i < 1000000; i++)
       asm("nop");
     SEGGER_RTT_printf(0, "Led_ON\r\n");
 
     // GPIOB->BSRR |= GPIO_BSRR_BR2;
     SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR2);
-    for (volatile uint32_t i = 0; i < 1000000; i++)
+    for ( i = 0; i < 1000000; i++)
       asm("nop");
     SEGGER_RTT_printf(0, "Led_OFF\r\n");
   }
 }
+/*************************** End of file ****************************/
+//**************************  Пример второй **************************
+
+/*************************** End of file ****************************/
